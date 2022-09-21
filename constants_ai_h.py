0df59f6c22897886c70e0280b41d288c2b3d8976
eriksjoarp <erik.westring@gmail.com>
2022-09-21 02:12:28 +0200

    Corrected import

--- constants_ai_h.py
+++ constants_ai_h.py
@@ -4,14 +4,14 @@ import sys, os
 BASE_DIR_GIT = r'C:\Users\erikw\git'
 DIR_HELPER = os.path.join(BASE_DIR_GIT, 'helper')
 
+
 sys.path.insert(1, DIR_HELPER)
-import helper as h
+import helpers as h
 
 
 RANDOM_STATE = 42
 BASE_DIR_AI = r'C:\ai'
 
 DIR_AI_HELPER = h.j(BASE_DIR_GIT, 'ai_helper')
-
 DIR_RESULTS = h.j(BASE_DIR_AI, 'results')
 
