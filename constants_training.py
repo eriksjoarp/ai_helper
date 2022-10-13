c8a4fa7f2122fd7d353a3a14183503ab60b5f68d
eriksjoarp <erik.westring@gmail.com>
2022-10-13 20:54:28 +0200

    added training constans used in huggingspace

---
+++ constants_training.py
@@ -0,0 +1,24 @@
+
+
+#########################                 huggingface vision models                #################################
+
+UNUSED_COLUMNS = 'remove_unused_columns'
+EVALUATION_STRATEGY = 'evaluation_strategy'
+SAVE_STRATEGY = 'save_strategy'
+LEARNING_RATE = 'learning_rate'
+PER_DEVICE_TRAIN_BATCH_SIZE = 'per_device_train_batch_size'
+GRADIENT_ACCUMULATION_STEPS = 'gradient_accumulation_steps'
+PER_DEVICE_EVAL_BATCH_SIZE = 'per_device_eval_batch_size'
+NUM_TRAIN_EPOCHS = 'num_train_epochs'
+WARMUP_RATIO = 'warmup_ratio'
+LOGGING_STEPS = 'logging_steps'
+LOAD_BEST_MODEL_AT_END = 'load_best_model_at_end'
+METRIC_FOR_BEST_MODEL = 'metric_for_best_model'
+PUSH_TO_HUB = 'push_to_hub'
+BF16 = 'bf16'
+
+
+
+
+
+
