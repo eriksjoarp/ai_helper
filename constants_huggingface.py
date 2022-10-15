e3e315ee4585eaab1e23787601dc742755427d72
eriksjoarp <erik.westring@gmail.com>
2022-10-15 04:13:21 +0200

    constants for many different swin models that can be loaded from huggingface

---
+++ constants_huggingface.py
@@ -0,0 +1,45 @@
+'''
+Constants regarding anything in huggingface
+Models
+
+'''
+
+
+#########################################               MODELS                  ###########################################
+
+# swin v1
+MODEL_VISION_SWIN_MICROSOFT_TINY_PATCH4_WINDOW7_224 = r'microsoft/swin-tiny-patch4-window7-224'
+
+MODEL_VISION_SWIN_MICROSOFT_SMALL_PATCH4_WINDOW7_224 = r'microsoft/swin-small-patch4-window7-224'
+
+MODEL_VISION_SWIN_MICROSOFT_BASE_PATCH4_WINDOW7_224 = r'microsoft/swin-base-patch4-window7-224'
+MODEL_VISION_SWIN_MICROSOFT_BASE_PATCH4_WINDOW12_224 = r'microsoft/swin-base-patch4-window12-384'
+MODEL_VISION_SWIN_MICROSOFT_BASE_PATCH4_WINDOW7_224_IN22K = r'microsoft/swin-base-patch4-window7-224-in22k'
+
+MODEL_VISION_SWIN_MICROSOFT_LARGE_PATCH4_WINDOW7_224 = r'microsoft/swin-large-patch4-window7-224'
+MODEL_VISION_SWIN_MICROSOFT_LARGE_PATCH4_WINDOW12_384 = r'microsoft/swin-large-patch4-window12-384'
+MODEL_VISION_SWIN_MICROSOFT_LARGE_PATCH4_WINDOW7_224_IN22K = r'microsoft/swin-large-patch4-window7-224-in22k'
+MODEL_VISION_SWIN_MICROSOFT_LARGE_PATCH4_WINDOW12_384_IN22K = r'microsoft/swin-large-patch4-window12-384-in22k'
+
+# swin v2
+MODEL_VISION_SWINV2_MICROSOFT_TINY_PATCH4_WINDOW8_256 = r'microsoft/swinv2-tiny-patch4-window8-256'
+MODEL_VISION_SWINV2_MICROSOFT_TINY_PATCH4_WINDOW16_256 = r'microsoft/swinv2-tiny-patch4-window16-256'
+
+MODEL_VISION_SWINV2_MICROSOFT_SMALL_PATCH4_WINDOW8_256 = r'microsoft/swinv2-small-patch4-window8-256'
+MODEL_VISION_SWINV2_MICROSOFT_SMALL_PATCH4_WINDOW16_256 = r'microsoft/swinv2-small-patch4-window16-256'
+
+MODEL_VISION_SWINV2_MICROSOFT_BASE_PATCH4_WINDOW8_256 = r'microsoft/swinv2-base-patch4-window8-256'
+MODEL_VISION_SWINV2_MICROSOFT_BASE_PATCH4_WINDOW16_256 = r'microsoft/swinv2-base-patch4-window16-256'
+MODEL_VISION_SWINV2_MICROSOFT_BASE_PATCH4_WINDOW12_192_22K = r'microsoft/swinv2-base-patch4-window12-192-22k'
+MODEL_VISION_SWINV2_MICROSOFT_BASE_PATCH4_WINDOW12TO16_192TO256_22K = r'microsoft/swinv2-base-patch4-window12to16-192to256-22k'
+
+MODEL_VISION_SWINV2_MICROSOFT_LARGE_PATCH4_WINDOW12_192_22K = r'microsoft/swinv2-large-patch4-window12-192-22k'
+MODEL_VISION_SWINV2_MICROSOFT_LARGE_PATCH4_WINDOW12TO16_192TO256_22KTO1K_FT = r'microsoft/swinv2-large-patch4-window12to16-192to256-22kto1k-ft'
+
+
+##########################################              hyper parameters optimizations              ########################################
+
+
+
+
+
